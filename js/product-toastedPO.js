2e8464d7dc3fa0ceb78beeaa2e61cb68ee2face8
MadiyarMukhambet <madiyarmuhambetov@gmail.com>
2024-11-01 14:07:17 +0500

    update

--- js/product-toastedPO.js
+++ js/product-toastedPO.js
@@ -53,3 +53,27 @@ showIngredientsButton.addEventListener('click', function() {
         });
     }
 });
+function checkAuth() {
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+    const signupContainer = document.querySelector('.signup');
+
+    if (isLoggedIn) {
+        signupContainer.innerHTML = `<button class="btn btn-outline-warning" onclick="logout()">Logout</button>`;
+    } else {
+        signupContainer.innerHTML = `
+            <li class="nav-item me-2"><a class="nav-link" href="login.html"><button class="btn btn-outline-warning"><i class="bi bi-person-circle"></i> Log</button></a></li>
+            <li class="nav-item"><a class="nav-link" href="register.html"><button type="button" class="btn btn-warning"><i class="fa-solid fa-key"></i> Register</button></a></li>
+        `;
+    }
+}
+
+// Функция выхода из аккаунта
+function logout() {
+    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('userEmail');
+    alert('You have been logged out.');
+    window.location.href = 'index.html';
+}
+
+// Выполнение проверки авторизации при загрузке страницы
+window.addEventListener('load', checkAuth);
